6695675c88ace8cce31719939593fd5c9746b057
Chen <chenyitian01@zju.edu.cn>
2026-03-15 10:50:43 +0800

    完成房间属性管理的API

--- web/web-admin/src/main/java/com/zju/lease/web/admin/controller/apartment/AttrController.java
+++ web/web-admin/src/main/java/com/zju/lease/web/admin/controller/apartment/AttrController.java
@@ -1,6 +1,7 @@
 package com.zju.lease.web.admin.controller.apartment;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.zju.lease.common.result.Result;
 import com.zju.lease.model.entity.AttrKey;
 import com.zju.lease.model.entity.AttrValue;
@@ -51,12 +52,19 @@ public class AttrController {
     @Operation(summary = "根据id删除属性名称")
     @DeleteMapping("key/deleteById")
     public Result removeAttrKeyById(@RequestParam Long attrKeyId) {
+        attrKeyService.removeById(attrKeyId);
+
+        LambdaQueryWrapper<AttrValue> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(AttrValue::getAttrKeyId, attrKeyId);
+        attrValueService.remove(queryWrapper);
+
         return Result.ok();
     }
 
     @Operation(summary = "根据id删除属性值")
     @DeleteMapping("value/deleteById")
     public Result removeAttrValueById(@RequestParam Long id) {
+        attrValueService.removeById(id);
         return Result.ok();
     }
 
