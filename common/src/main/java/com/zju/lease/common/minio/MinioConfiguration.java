ef75239c5aa03a8c734e1ab31feba78b904d6c53
Chen <chenyitian01@zju.edu.cn>
2026-03-15 15:04:11 +0800

    完成上传文件有关的API

---
+++ common/src/main/java/com/zju/lease/common/minio/MinioConfiguration.java
@@ -0,0 +1,24 @@
+package com.zju.lease.common.minio;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("com.zju.lease.common.minio")
+public class MinioConfiguration {
+
+    @Autowired
+    private MinioProperties properties;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(properties.getEndpoint())
+                .credentials(properties.getAccessKey(), properties.getSecretKey())
+                .build();
+    }
+}
