e198dbec042ee1db21863b5e2324c98352a92fba
Chen <chenyitian01@zju.edu.cn>
2026-03-16 14:52:08 +0800

    完成根据ID修改公寓状态，根据区县ID查询公寓信息列表

--- web/web-admin/src/main/java/com/zju/lease/web/admin/controller/apartment/ApartmentController.java
+++ web/web-admin/src/main/java/com/zju/lease/web/admin/controller/apartment/ApartmentController.java
@@ -1,6 +1,8 @@
 package com.zju.lease.web.admin.controller.apartment;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zju.lease.common.result.Result;
 import com.zju.lease.model.entity.ApartmentInfo;
@@ -62,13 +64,20 @@ public class ApartmentController {
     @Operation(summary = "根据id修改公寓发布状态")
     @PostMapping("updateReleaseStatusById")
     public Result updateReleaseStatusById(@RequestParam Long id, @RequestParam ReleaseStatus status) {
+        LambdaUpdateWrapper<ApartmentInfo> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(ApartmentInfo::getId, id);
+        updateWrapper.set(ApartmentInfo::getIsRelease, status);
+        service.update(updateWrapper);
         return Result.ok();
     }
 
     @Operation(summary = "根据区县id查询公寓信息列表")
     @GetMapping("listInfoByDistrictId")
     public Result<List<ApartmentInfo>> listInfoByDistrictId(@RequestParam Long id) {
-        return Result.ok();
+        LambdaQueryWrapper<ApartmentInfo> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ApartmentInfo::getDistrictId, id);
+        List<ApartmentInfo> list = service.list(queryWrapper);
+        return Result.ok(list);
     }
 }
 
