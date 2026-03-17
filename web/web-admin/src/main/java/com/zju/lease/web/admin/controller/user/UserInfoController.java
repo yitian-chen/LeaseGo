65125ee1734c3429cffa54e9fec7b5a6e225118c
Chen <chenyitian01@zju.edu.cn>
2026-03-17 16:18:23 +0800

    完成分页查询用户信息，在@TableField注解中添加select = false以规避查询密码

--- web/web-admin/src/main/java/com/zju/lease/web/admin/controller/user/UserInfoController.java
+++ web/web-admin/src/main/java/com/zju/lease/web/admin/controller/user/UserInfoController.java
@@ -1,13 +1,17 @@
 package com.zju.lease.web.admin.controller.user;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zju.lease.common.result.Result;
 import com.zju.lease.model.entity.UserInfo;
 import com.zju.lease.model.enums.BaseStatus;
+import com.zju.lease.web.admin.service.UserInfoService;
 import com.zju.lease.web.admin.vo.user.UserInfoQueryVo;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "用户信息管理")
@@ -15,10 +19,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/user")
 public class UserInfoController {
 
+    @Autowired
+    private UserInfoService service;
+
     @Operation(summary = "分页查询用户信息")
     @GetMapping("page")
     public Result<IPage<UserInfo>> pageUserInfo(@RequestParam long current, @RequestParam long size, UserInfoQueryVo queryVo) {
-        return Result.ok();
+        Page<UserInfo> page = new Page<>(current, size);
+        LambdaQueryWrapper<UserInfo> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(queryVo.getPhone() != null, UserInfo::getPhone, queryVo.getPhone());
+        queryWrapper.eq(queryVo.getStatus() != null, UserInfo::getStatus, queryVo.getStatus());
+        Page<UserInfo> result = service.page(page, queryWrapper);
+        return Result.ok(result);
     }
 
     @Operation(summary = "根据用户id更新账号状态")
