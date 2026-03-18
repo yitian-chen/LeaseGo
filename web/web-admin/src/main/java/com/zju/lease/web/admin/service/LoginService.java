f833b45af8c9d31d2b07c2c9fbc68ede9c49ab73
Chen <chenyitian01@zju.edu.cn>
2026-03-18 09:10:10 +0800

    完成获取图片验证码接口，调用EasyCaptcha，将UUID(key)与验证码code存入Redis并设置过期时间

--- web/web-admin/src/main/java/com/zju/lease/web/admin/service/LoginService.java
+++ web/web-admin/src/main/java/com/zju/lease/web/admin/service/LoginService.java
@@ -1,5 +1,8 @@
 package com.zju.lease.web.admin.service;
 
+import com.zju.lease.web.admin.vo.login.CaptchaVo;
+
 public interface LoginService {
 
+    CaptchaVo getCaptcha();
 }
--- web/web-admin/src/main/java/com/zju/lease/web/admin/service/impl/LoginServiceImpl.java
+++ web/web-admin/src/main/java/com/zju/lease/web/admin/service/impl/LoginServiceImpl.java
@@ -1,9 +1,33 @@
 package com.zju.lease.web.admin.service.impl;
 
+import com.wf.captcha.SpecCaptcha;
+import com.zju.lease.common.constant.RedisConstant;
 import com.zju.lease.web.admin.service.LoginService;
+import com.zju.lease.web.admin.vo.login.CaptchaVo;
+import org.checkerframework.checker.units.qual.A;
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class LoginServiceImpl implements LoginService {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public CaptchaVo getCaptcha() {
+        SpecCaptcha specCaptcha = new SpecCaptcha(130, 48, 4);
+
+        String code = specCaptcha.text().toLowerCase();
+        String key = RedisConstant.ADMIN_LOGIN_PREFIX + UUID.randomUUID();
+
+        stringRedisTemplate.opsForValue().set(key, code, RedisConstant.ADMIN_LOGIN_CAPTCHA_TTL_SEC, TimeUnit.SECONDS);
+
+        return new CaptchaVo(specCaptcha.toBase64(),key);
+    }
 }
