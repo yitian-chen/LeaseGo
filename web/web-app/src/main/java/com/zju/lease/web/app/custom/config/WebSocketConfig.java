3b48e7fb374ee79b0fae53d7317c5a73bc25b255
Chen <chenyitian01@zju.edu.cn>
2026-04-01 14:20:50 +0800

    fix: 调整了项目结构，目前可以成功编译

--- web/web-app/src/main/java/com/zju/lease/web/app/custom/config/WebSocketConfig.java
+++ web/web-app/src/main/java/com/zju/lease/web/app/custom/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.zju.lease.web.app.custom.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
@@ -7,6 +8,7 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 @Configuration
 public class WebSocketConfig {
     @Bean
+    @ConditionalOnProperty(name = "spring.profiles.active", havingValue = "test", matchIfMissing = false)
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
